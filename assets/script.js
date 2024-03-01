1c9377115bfb1ee4cb03b22c626be44aac8e4a9e
PhuongNam277 <namp3411@gmail.com>
2024-03-01 20:31:44 +0700

    finish all project

---
+++ assets/script.js
@@ -0,0 +1,28 @@
+// Sidebar item js
+const menuOpen = document.getElementById('menu-open');
+const menuClose = document.getElementById('menu-close');
+const sideBar = document.querySelector('.container .left-section');
+const sideBarItems = document.querySelectorAll('.container .left-section .sidebar .item');
+
+// Làm cho menu bật lên
+menuOpen.addEventListener('click', () => {
+    sideBar.style.top = '0';
+});
+
+// Làm cho menu đóng vào
+menuClose.addEventListener('click', () => {
+    sideBar.style.top = '-60vh';
+});
+
+let activeItem = sideBarItems[0];
+
+sideBarItems.forEach(element => {
+    element.addEventListener('click', () => {
+        if(activeItem) {
+            activeItem.removeAttribute('id');
+        }
+
+        element.setAttribute('id', 'active');
+        activeItem = element;
+    });
+});
